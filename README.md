0a17a9539633886395437408b5347413b19b7086
Tuschl <tuschl@digitalmobil.com>
2016-09-14 12:20:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wordpress Build Tools
+
+A docker image that contains all build tools required for building dm-wordpress images.
+
+**TODO**: Add details
---
+++ wp-build/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DATA_UID=$(stat -c "%u" /data)
+
+if [ $DATA_UID -gt 0 ]; then
+    echo "Executing as UID $DATA_UID"
+    usermod -u $DATA_UID wp-build >/dev/null 2>&1
+    chown -R wp-build /home/wp-build
+    sudo -u wp-build "$@"
+else
+    exec "$@"
+fi
